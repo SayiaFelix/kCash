a7e6fb34fc1e2f62c7d1a5f43c45775c1c981fd0
SayiaFelix <sayiafelix18@gmail.com>
2022-10-26 15:35:07 +0300

    styling signup

--- KryptoPesa/src/app/app-routing.module.ts
+++ KryptoPesa/src/app/app-routing.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { KryptoPriceComponent } from './krypto-price/krypto-price.component';
 import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
+  
   { path: '', redirectTo:"/homepage", pathMatch:"full"},
   
-  {path:'login',component:LoginComponent,},
+  {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
+  {path:'homepage',component:HomepageComponent},
   {path:'dashboard',component:DashboardComponent},
   {path:'about-us',component:AboutusComponent},
   {path:'Krypto-Price',component:KryptoPriceComponent},
