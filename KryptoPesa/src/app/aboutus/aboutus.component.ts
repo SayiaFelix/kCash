a0ad5e42bd4d0072dc44e6d99035080328a1a01d
SayiaFelix <sayiafelix18@gmail.com>
2022-10-26 14:30:44 +0300

    adding about us

--- KryptoPesa/src/app/aboutus/aboutus.component.ts
+++ KryptoPesa/src/app/aboutus/aboutus.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-aboutus',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutus.component.css']
 })
 export class AboutusComponent implements OnInit {
-
-  constructor() { }
+  public contactForm!: FormGroup; 
+  isSubmit =true;
+  submittedMessage='';
+  constructor(private fb:FormBuilder,
+     private http:HttpClient,
+      private router:Router,
+      private toast: NgToastService
+      ){ }
 
   ngOnInit(): void {
+    this.contactForm= this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required,Validators.email]],
+      message: ['', Validators.required]
+    });
   }
+
+  sendMessage(value:any){
+  console.log(value)
+  
+  this.isSubmit = true;
+  this.submittedMessage ='Submitted Successfully';
+  
+  setTimeout(()=>{
+    this.isSubmit = false;
+  },5000);
+  }
+
 
 }
