b4d9d914cf016b41d6e905cfbeb96a331136df2d
SayiaFelix <sayiafelix18@gmail.com>
2022-10-26 14:39:13 +0300

    adding sign up

--- KryptoPesa/src/app/login/login.component.ts
+++ KryptoPesa/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public LoginForm!: FormGroup; 
 
-  ngOnInit(): void {
+  constructor(private fb:FormBuilder,
+     private http:HttpClient,
+      private router:Router,
+      private toast: NgToastService
+      ) { }
+  
+  login(){
+    if(this.LoginForm.valid){
+      // send obj to db
+      console.log(this.LoginForm.value);
+      this.http.get<any>("http://localhost:3000/users/")
+      .subscribe(res=>{
+        const user = res.find((a:any)=>{
+  
+        return a.email === this.LoginForm.value.email &&
+         a.password === this.LoginForm.value.password
+        });
+        if(user){
+          // alert('Login Successfully');
+          this.toast.success({detail:'SUCCESS!!!!',summary:"Login Successfully!!",duration:5000})
+          // this.toast.info({detail:'Welcome Message',summary:"Welcome to our Dashboard!!",duration:5000})
+          this.LoginForm.reset();
+          this.router.navigate(['dashboard']);
+        }
+        else{
+          this.toast.error({detail:'FAILED!!!',summary:"Login Failed!!",duration:5000})
+          // this.toast.warning({detail:'Warming Message',summary:"User Not Found,Use Correct Details!!",duration:5000})
+          // alert('User not found')
+        } 
+      },
+      err=>{
+        alert('Something went Wrong ,Try again!!!!!')
+      })
+      
+    }
+    else{
+      this.validateAllFormFields(this.LoginForm)
+      alert('Form is invalid')
+    }
   }
+  
+  private validateAllFormFields(formGroup:FormGroup)
+  {
+  Object.keys(formGroup.controls).forEach(field=>{
+    const control = formGroup.get(field);
+    if( control instanceof FormControl){
+      control.markAsDirty( {onlySelf: true});
+    }
+  
+    else if( control instanceof FormGroup){
+      this.validateAllFormFields(control);
+    }
+  })
+  
+  
+  }
+    ngOnInit(): void {
+      this.LoginForm= this.fb.group({
+        email: ['', [Validators.required,Validators.email]],
+        password: ['', Validators.required]
+      });
+    }
 
 }
--- KryptoPesa/src/app/signup/signup.component.ts
+++ KryptoPesa/src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  public SignupForm!: FormGroup; 
+  constructor
+  (
+    private fb:FormBuilder,
+    private http:HttpClient,
+    private router:Router,
+    private toast:NgToastService
+    ) { }
 
   ngOnInit(): void {
+    this.SignupForm= this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  })
+
+  }
+  Signup(){
+    if(this.SignupForm.valid){
+      console.log(this.SignupForm.value);
+      this.http.post<any>("http://localhost:3000/users/",this.SignupForm.value)
+      .subscribe(res=>{
+        this.toast.success({detail:'Success Message',summary:"Sign In Successfully!!",duration:5000})
+        // alert('Signup successfully');
+        this.SignupForm.reset();
+       this.router.navigate(['login']);
+       },err=>{
+        this.toast.error({detail:'Failed Message',summary:"Sign In Failed, Something Went wrong!!",duration:5000})
+        // alert('something went wrong')
+  
+     })
+      // send obj to db
+  
+    }else{
+      // console.log('Form is not valid')
+      // through an error
+  
+      this.validateAllFormFields(this.SignupForm)
+      alert('Form is  invalid')
+    }
+  
+  }
+  
+  private validateAllFormFields(formGroup:FormGroup)
+  {
+  Object.keys(formGroup.controls).forEach(field=>{
+    const control = formGroup.get(field);
+  
+    if( control instanceof FormControl){
+      control.markAsDirty( {onlySelf: true});
+    }
+    else if( control instanceof FormGroup){
+      this.validateAllFormFields(control);
+    }
+  })
+    
   }
 
 }
