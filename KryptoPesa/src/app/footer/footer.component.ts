7fa5ac6da37a115c4e877bfaf65b44f8821f1345
SayiaFelix <sayiafelix18@gmail.com>
2022-10-27 17:17:13 +0300

    styling signup

--- KryptoPesa/src/app/footer/footer.component.ts
+++ KryptoPesa/src/app/footer/footer.component.ts
@@ -30,7 +30,7 @@ export class FooterComponent implements OnInit {
   console.log(value)
   this.isSubmit = true;
   this.submittedMessage ='Submitted Successfully';
-  this.toast.success({detail:'SUCCESS!!!!',summary:"SUBSCRIBED SUCCESSFULLY!!",duration:3000})
+  this.toast.success({detail:'SUCCESS!!!!',summary:"Subscribe Successfully!!",duration:3000})
 
   setTimeout(()=>{
     this.isSubmit = false;
